e3d57ba567ceb81a3eaf0b1a10908b7cbffd695f
Rene Noriega <dorian.animal@gmail.com>
2020-04-20 22:27:17 -0500

    Estructura base

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var contactRouter = require("./routes/contact");
+var aboutRouter = require("./routes/about");
 
 var app = express();
 
@@ -20,11 +21,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/contact" , contactRouter);
+app.use("/about" , aboutRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function(req,res, next){
+  res.status(404);
+  res.render("errors/404.pug", {detalle: "Pagina no encontrada"});
 });
 
 // error handler
